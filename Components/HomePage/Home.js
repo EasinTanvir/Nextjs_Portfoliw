e3528279188f9212a78ce015a5e761b0805cdaba
EasinTanvir <easintanvir102@gmail.com>
2022-06-05 10:45:31 +0600

    update data

--- Components/HomePage/Home.js
+++ Components/HomePage/Home.js
@@ -7,7 +7,7 @@ export default function Homes(){
        <div  className={classes.image}>
          <Image src='/images/side/tanvir.jpg' alt='easintanvir' width={300} height={300}/>
        </div>
-       <h1>Hi!! I am Easin</h1>
+       <h1>Hi, I am Easin</h1>
        <p>I blog about web development - especially frontend frameworks like React or Angular</p>
    </section>
   )
